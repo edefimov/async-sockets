44e0d096e14243f39351a4c87d233e09b6077310
edefimov <edefimov.it@gmail.com>
2015-08-18 22:59:12 +0300

    Don't use sudo in travis

--- travis-install.sh
+++ travis-install.sh
@@ -4,8 +4,6 @@ set -e
 if [[ "$TRAVIS_PHP_VERSION" != "hhvm" &&
       "$TRAVIS_PHP_VERSION" != "7.0" &&
       "$TRAVIS_PHP_VERSION" != "nightly" ]]; then
-    sudo apt-get install -y libevent-dev
-
     echo "yes" | pecl install event
 
     # install 'libevent' PHP extension
