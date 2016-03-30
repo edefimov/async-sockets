260fff584005fade0abb46b4cb8b06941c2dcf84
Evgenij Efimov <edefimov.it@gmail.com>
2016-03-30 21:16:23 +0300

    Changed year in files' headers

--- tests/unit/Operation/WriteOperationTest.php
+++ tests/unit/Operation/WriteOperationTest.php
@@ -2,7 +2,7 @@
 /**
  * Async sockets
  *
- * @copyright Copyright (c) 2015, Efimov Evgenij <edefimov.it@gmail.com>
+ * @copyright Copyright (c) 2015-2016, Efimov Evgenij <edefimov.it@gmail.com>
  *
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
